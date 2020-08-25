b6e434f8599530f211770d6e46a5c4e4bacae1dc
Nikolaus Ferdy Tanujaya <nikz.ferdy@gmail.com>
2020-08-25 23:46:15 +0700

    Recommendation #4: add Serial (auto increment integer) and primary key to column id table customers and customer_addresses

--- schema.sql
+++ schema.sql
@@ -8,14 +8,14 @@ INSERT INTO administrators (id, username, password) VALUES
 (1, 'admin', 'admin');
 
 CREATE TABLE customers (
-    id int,
+    id serial primary key,
     name varchar(200),
     phone varchar(200)
 );
 
 
 CREATE TABLE customer_addresses (
-   id int,
+   id serial primary key,
    customer_id int,
    address varchar(200),
    zipcode varchar(5)
